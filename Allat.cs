9391dc1f92880144a5d730f0a05ab5d7688834c5
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2024-06-04 08:03:15 +0200

    Add files via upload

---
+++ Allat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petApp
+{
+    internal class Allat
+    {
+        public string Fajta { get; set; }
+        public int Eves { get; set; }
+        public string Szine { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Allat(string sor)
+        {
+            var atmeneti = sor.Split(';');
+            Fajta = atmeneti[0];
+            Eves = Convert.ToInt32(atmeneti[1]);
+            Szine = atmeneti[2];
+            ImageUrl = atmeneti[3];
+        }
+
+        public override string ToString()
+        {
+            return $"Fajta: {this.Fajta}\nKora: {this.Eves}\nSzíne: {this.Szine}";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace petApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Allat> allatok;
+        public MainWindow()
+        {
+            InitializeComponent();
+            allatok = new List<Allat>();
+            Beolvasas();
+
+            AllatokDG.ItemsSource = allatok;
+        }
+
+        public static void Beolvasas() 
+        {
+            try
+            {
+                StreamReader sr = new(@"..\..\..\src\animals.txt");
+                while (!sr.EndOfStream)
+                {
+                    allatok.Add(new Allat(sr.ReadLine()));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Hiba a fájl beolvasása során!","Hiba",MessageBoxButton.OK,MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void Kedveles(object sender, RoutedEventArgs e)
+        {
+            if (AllatokDG.SelectedItem == null)
+            {
+                MessageBox.Show("Válassz ki egy állatot!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else 
+            {
+                if (KedvencekLB.Items.Contains(AllatokDG.SelectedItem))
+                {
+                    MessageBox.Show("Már egyszer hozzáadtad a kedvencekhez ezt az állatot!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    KedvencekLB.Items.Add(AllatokDG.SelectedItem);
+                }
+            }
+        }
+
+        private void Torles(object sender, RoutedEventArgs e)
+        {
+            if (KedvencekLB.SelectedItem == null)
+            {
+                MessageBox.Show("Válassz ki egy állatot!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else 
+            {
+                KedvencekLB.Items.Remove(KedvencekLB.SelectedItem);
+            }
+        }
+
+        private void AllatokDG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AllatokDG.SelectedItem is Allat selected)
+            {
+                AllatImage.Source = new BitmapImage(new Uri(@$"\{selected.ImageUrl}", UriKind.Relative));
+            }
+        }
+
+    }
+}
